d34d6a969b908e65ef2248a4b6314535b7221d57
theushub <53832627+theushub@users.noreply.github.com>
2021-05-19 09:49:53 -0300

    modificações no JavaBeans

--- agenda/src/model/JavaBeans.java
+++ agenda/src/model/JavaBeans.java
@@ -6,7 +6,106 @@ package model;
  * @author matheus cesarini
  *
  */
-
 public class JavaBeans {
+	// variáveis encapsuladas usadas no fluxo
+	private String idcon;
+	private String nome;
+	private String fone;
+	private String email;
+
+	/**
+	 * idcon
+	 * 
+	 * @return idcon
+	 */
+	public String getIdcon() {
+		return idcon;
+	}
+
+	/**
+	 * idcon
+	 * 
+	 * @param idcon
+	 */
+	public void setIdcon(String idcon) {
+		this.idcon = idcon;
+	}
+
+	/**
+	 * nome
+	 * 
+	 * @return nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * nome
+	 * 
+	 * @param nome
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * fone
+	 * 
+	 * @return fone
+	 */
+	public String getFone() {
+		return fone;
+	}
+
+	/**
+	 * fone
+	 * 
+	 * @param fone
+	 */
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	/**
+	 * emial
+	 * 
+	 * @return email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * email
+	 * 
+	 * @param email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * construtor
+	 */
+	public JavaBeans() {
+		super();
+	}
+
+	/**
+	 * construtor para uso do Arraylist
+	 * 
+	 * @param idcon
+	 * @param nome
+	 * @param fone
+	 * @param email
+	 */
+	public JavaBeans(String idcon, String nome, String fone, String email) {
+		super();
+		this.idcon = idcon;
+		this.nome = nome;
+		this.fone = fone;
+		this.email = email;
+	}
 
 }
