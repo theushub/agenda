65b3cb5d5e408192e3bf189214ddae9a86fe7883
theushub <53832627+theushub@users.noreply.github.com>
2021-05-19 09:47:11 -0300

    adicionado arquivos do projeto JavaBeans

---
+++ agenda/src/model/DAO.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * camada model do modelo mvc
+ * 
+ * @author matheus cesarini
+ *
+ */
+
+public class DAO {
+
+}
---
+++ agenda/src/model/JavaBeans.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Encapsulamneto dos dados (fluxo)
+ * 
+ * @author matheus cesarini
+ *
+ */
+
+public class JavaBeans {
+
+}
